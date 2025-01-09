12928c20a951d951a3913b5f96bf7902544ec798
Justin Luke <justin.luke@berkeley.edu>
2025-01-09 10:16:24 -0800

    ENH: Updated field names in EVSE_parameters.csv and refactored how EVSE costs are ingested accordingly.

--- pamodpy/EVSE.py
+++ pamodpy/EVSE.py
@@ -18,9 +18,9 @@ class EVSE():
         self.year = df.loc[idx]['year'].values[0]
         self.power_type = df.loc[idx]['power_type'].values[0]           # ['AC', 'DC']
         self.rate = df.loc[idx]['rate'].values[0]                       # [kW]
-        self.evse_unit_cost = df.loc[idx]['evse_unit_cost'].values[0]   # [$]
-        self.p_infra_marginal = self.evse_unit_cost + (84 + 240)/2 + (200 + 250)/2
-        self.p_infra_capital = 0.1 * (df.loc[idx]['capital_10unit_cost'].values[0] + (1500+3500)/2 + (325+1000)/2)
+        self.evse_unit_cost = df.loc[idx]['per_plug_hardware_cost'].values[0] + df.loc[idx]['per_plug_installation_cost'].values[0]   # [$]
+        self.p_infra_marginal = self.evse_unit_cost #+ (84 + 240)/2 + (200 + 250)/2
+        self.p_infra_capital = df.loc[idx]['other_per_site_costs'].values[0] #0.1 * (df.loc[idx]['capital_10unit_cost'].values[0] + (1500+3500)/2 + (325+1000)/2)
         self.plugs_per_unit = 1
         self.num_plugs = self.plugs_per_unit * self.num_units
         if self.station is not None:
