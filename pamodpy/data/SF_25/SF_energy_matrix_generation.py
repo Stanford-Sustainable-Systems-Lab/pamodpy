b5714b3e592dd1431b792a46f6965bebe985e466
Justin Luke <justin.luke@berkeley.edu>
2025-02-23 22:27:55 -0800

    New Sherlock experiment configs for 185Whpkm energy consump experiment

--- pamodpy/data/SF_25/SF_energy_matrix_generation.py
+++ pamodpy/data/SF_25/SF_energy_matrix_generation.py
@@ -7,35 +7,36 @@ distance_matrix = np.load('distance_matrix.npy')
 
 eta_charge = 0.90
 
-kwh_per_mi = 135 / 1000 * 1.609344 * eta_charge
+wh_per_km = 185
+kwh_per_mi = wh_per_km / 1000 * 1.609344 * eta_charge
 print(eta_charge / kwh_per_mi)
 energy_matrix = distance_matrix * kwh_per_mi
-# np.save('energies_2024_Dacia_Spring_Electric_65_Expression.npy', energy_matrix)
+np.save(f'energies_{wh_per_km}Whpkm_25kWh.npy', energy_matrix)
 
 # ioniqhyb_gal_per_mi = 1 / 52.3
 # ioniqhyb_energy_matrix = distance_matrix * ioniqhyb_gal_per_mi
 # np.save('energies_2021_Hyundai_IONIQ_Hybrid_SE.npy', ioniqhyb_energy_matrix)
 
-od_matrix = np.load('od_matrix.npy')
-od_matrix[25:, 25:, :] = 0
-duration_matrix = np.load('duration_matrix.npy') / (60 * 60)
-duration_matrix2 = duration_matrix.copy()
-distance_matrix2 = distance_matrix.copy()
-
-distance_matrix2[25, :, :] += 13
-distance_matrix2[:, 25, :] += 13
-duration_matrix2[25, :, :] += 20 / 60
-duration_matrix2[:, 25, :] += 20 / 60
-
-distance_matrix2[26, :, :] += 10
-distance_matrix2[:, 26, :] += 10
-duration_matrix2[26, :, :] += 25 / 60
-duration_matrix2[:, 26, :] += 25 / 60
-
-distance_matrix2[27, :, :] += 15
-distance_matrix2[:, 27, :] += 15
-duration_matrix2[27, :, :] += 23 / 60
-duration_matrix2[:, 27, :] += 23 / 60
-
-energy_matrix2 = distance_matrix2 * kwh_per_mi + duration_matrix2 * 0
-print("20th percentile = {} kWh".format(np.percentile(np.repeat(energy_matrix2[energy_matrix2 > 0].flatten(), np.round(od_matrix[energy_matrix2 > 0].flatten()).astype(int)), 20)))
+# od_matrix = np.load('od_matrix.npy')
+# od_matrix[25:, 25:, :] = 0
+# duration_matrix = np.load('duration_matrix.npy') / (60 * 60)
+# duration_matrix2 = duration_matrix.copy()
+# distance_matrix2 = distance_matrix.copy()
+#
+# distance_matrix2[25, :, :] += 13
+# distance_matrix2[:, 25, :] += 13
+# duration_matrix2[25, :, :] += 20 / 60
+# duration_matrix2[:, 25, :] += 20 / 60
+#
+# distance_matrix2[26, :, :] += 10
+# distance_matrix2[:, 26, :] += 10
+# duration_matrix2[26, :, :] += 25 / 60
+# duration_matrix2[:, 26, :] += 25 / 60
+#
+# distance_matrix2[27, :, :] += 15
+# distance_matrix2[:, 27, :] += 15
+# duration_matrix2[27, :, :] += 23 / 60
+# duration_matrix2[:, 27, :] += 23 / 60
+#
+# energy_matrix2 = distance_matrix2 * kwh_per_mi + duration_matrix2 * 0
+# print("20th percentile = {} kWh".format(np.percentile(np.repeat(energy_matrix2[energy_matrix2 > 0].flatten(), np.round(od_matrix[energy_matrix2 > 0].flatten()).astype(int)), 20)))
